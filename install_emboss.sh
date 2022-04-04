9022ee6622b899804e9fabadec57edc4118493ee
jomare1188 <jomare1188@gmail.com>
2022-04-04 14:38:47 -0300

    fix git part

--- install_emboss.sh
+++ install_emboss.sh
@@ -3,7 +3,7 @@ sudo apt upgrade -y
 sudo apt install git
 sudo apt install emboss* -y
 cp /usr/share/EMBOSS/emboss.standard /usr/share/EMBOSS/emboss.standard.copy
-https://github.com/jomare1188/install_emboss.git
+git clone https://github.com/jomare1188/install_emboss.git
 cd install_emboss
 cp emboss.default.template /usr/share/EMBOSS/emboss.standard
 dbshow
